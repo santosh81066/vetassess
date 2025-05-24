1a8abe7d90dde2f0fef861df7648774beaa895f1
Muzammil <md.muzammil939@gmail.com>
2025-05-24 18:36:26 +0530

    updated

--- lib/widgets/under_maintenance.dart
+++ lib/widgets/under_maintenance.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:vetassess/widgets/BasePageLayout.dart';
 
 class MaintenancePage extends StatelessWidget {
-  const MaintenancePage({Key? key}) : super(key: key);
+  const MaintenancePage({super.key});
 
   @override
   Widget build(BuildContext context) {
